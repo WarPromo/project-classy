ef858d8c374c1dfcdb8e1cb2a11af99543ded406
WarPromo <56988049+WarPromo@users.noreply.github.com>
2023-09-05 16:08:36 -0700

    file generation

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ io.attach(httpServer);
 
 
 //create a dev pass key so you can delete bad comments
+
+if(!fs.existsSync("./storage/devpass.txt")) fs.writeFileSync("./storage/devpass.txt", "changethispassword");
+
 let devPass = fs.readFileSync("./storage/devpass.txt", 'utf8');
 
 import { ProfanityEngine } from '@coffeeandfun/google-profanity-words';
 
 const profanity = new ProfanityEngine({ language: 'en' });
 
+if(!fs.existsSync("./storage/classlist.json")) generateStorage();
 
 let classlist = JSON.parse(fs.readFileSync("./storage/classlist.json", "utf8"));
 let classcomments = JSON.parse(fs.readFileSync("./storage/classcomments.json", "utf8"));
@@ -265,5 +269,51 @@ async function validComment(comment){
 
 
   return true;
+
+}
+
+
+function generateStorage(){
+
+  console.log("Generating Storage.");
+
+  let file = fs.readFileSync("./storage/descs.txt", "utf8");
+
+  file = file.split("\r\n\r\n");
+
+  let classes = [];
+  let classcomments = {};
+
+  for(var i = 0; i < file.length; i++){
+
+    file[i] = file[i].split("\r\n");
+
+    let classname = file[i][0];
+    let id = classname.split(" ").join("").split("-").join("").toLowerCase();
+    let classdesc = file[i][1];
+    let classtags = file[i][2].split(" ");
+
+    let object = {
+
+      name: classname,
+      id: id,
+      description: classdesc,
+      rating: {
+        Enjoyment: 0,
+        Difficulty: 0,
+        Workload: 0,
+        Usefulness: 0
+      },
+      tags: classtags
+
+    }
+
+    classes.push(object);
+    classcomments[id] = [];
+
+  }
+
+  fs.writeFileSync("./storage/classlist.json", JSON.stringify(classes))
+  fs.writeFileSync("./storage/classcomments.json", JSON.stringify(classcomments))
 
 }
